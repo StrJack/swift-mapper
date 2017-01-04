64fc73174ae24b4f33e19c12cb0b709953aa5b5c
Eugene Stroganov <estroganov@enova.com>
2017-01-04 16:28:45 -0600

    framework project for swift-mapper added

---
+++ SwiftMapper/SwiftMapper.h
@@ -0,0 +1,19 @@
+//
+//  SwiftMapper.h
+//  SwiftMapper
+//
+//  Created by Yevhen Strohanov on 1/4/17.
+//  Copyright © 2017 JackTech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftMapper.
+FOUNDATION_EXPORT double SwiftMapperVersionNumber;
+
+//! Project version string for SwiftMapper.
+FOUNDATION_EXPORT const unsigned char SwiftMapperVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftMapper/PublicHeader.h>
+
+
